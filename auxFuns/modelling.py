8734d897a1fd69a04c4eb98f549d71cd62cd61f8
Angel Garcia <angarcia@student.ethz.ch>
2023-06-21 17:19:29 +0200

    updated auxfuns

--- auxFuns/modelling.py
+++ auxFuns/modelling.py
@@ -9,6 +9,8 @@ from sklearn.metrics import f1_score, make_scorer, confusion_matrix, roc_auc_sco
 from sklearn.preprocessing import FunctionTransformer, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from category_encoders import OneHotEncoder
+
 
 def preprocess_rsv (df1, input_test_size = 0.2, random_seed = 42):
 
@@ -20,10 +22,8 @@ def preprocess_rsv (df1, input_test_size = 0.2, random_seed = 42):
     numeric_features_left = ['sine', 'cosine']
 
     # 2. Define transformers for every feature type and build the preprocessor
-    # 2.1 First, dummy encode categoricals
-    df1 = pd.get_dummies(df1, columns=categorical_features, drop_first= True)
+    categorical_transformer = OneHotEncoder(drop_invariant=True, )
 
-    # 2.2. Second, transform the numerical features
     right_transformer = Pipeline(steps=[
         ('log', FunctionTransformer(np.log1p, validate=False)),
         ('scaler', StandardScaler())
@@ -36,6 +36,7 @@ def preprocess_rsv (df1, input_test_size = 0.2, random_seed = 42):
 
     preprocessor = ColumnTransformer(
         transformers=[
+            ('cat', categorical_transformer, categorical_features),
             ('num_right', right_transformer, numeric_features_right),
             ('num_left', left_transformer, numeric_features_left)
         ])
@@ -48,4 +49,69 @@ def preprocess_rsv (df1, input_test_size = 0.2, random_seed = 42):
     X_train_transformed = preprocessor.fit_transform(X_train)
     X_test_transformed = preprocessor.transform(X_test)
 
-    return X_train_transformed, y_train, X_test_transformed, y_test
+    return X_train_transformed, y_train, X_test_transformed, y_test, preprocessor
+
+def train_model_rsv(model, param_grid, target_scorer, n_cv_folds,
+                    X_train, y_train):
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring=target_scorer, cv=n_cv_folds)
+
+    print(f'Training model ... {model}')
+
+    grid_search.fit(X_train, y_train)
+
+    print('Best training parameters: ', grid_search.best_params_)
+    print('Best training f1-score: ', grid_search.best_score_)
+
+    return grid_search
+
+
+
+def calculate_performance_metrics_rsv(trained_model, X_test, y_test, print_roc = True):
+    # 1. First, compute predictions of the model, both pointwise and in probabilities
+    y_pred = trained_model.predict(X_test)
+    y_probs = trained_model.predict_proba(X_test)[:, 1]
+
+    # 2. Calculate the confusion matrix metrics
+    cm = confusion_matrix(y_test, y_pred)
+    tn = cm[0, 0]
+    fp = cm[0, 1]
+    fn = cm[1, 0]
+    tp = cm[1, 1]
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    sensitivity = recall
+    specificity = tn / (tn + fp)
+    ppv = precision
+    npv = tn / (tn + fn)
+    accuracy = (tp + tn)/(tp + tn + fp + fn)
+    f1 = (2*precision*recall)/(precision + recall)
+
+    # 3. Calculate the roc curve
+    aux_y_test = [1 if label == 'Positive' else 0 for label in y_test]
+    fpr, tpr, thresholds = roc_curve(aux_y_test, y_probs)
+
+    auc_score = roc_auc_score(aux_y_test, y_probs)
+
+    # 4. Print metrics and (if requested) the ROC Curve
+    print(f'AUC Score: {auc_score}')
+    print(f'Precision / Positive predictive value: {precision}')
+    print(f'Specificity: {specificity}')
+    print(f'Recall / sensitivity: {recall}')
+    print(f'Negative predictive value: {npv}')
+    print(f'Accuracy: {accuracy}')
+    print(f'F-1: {f1}')
+
+    if print_roc:
+        plt.figure()
+        plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % auc_score)
+        plt.plot([0, 1], [0, 1], 'k--')  # random predictions curve
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate or (1 - Specifity)')
+        plt.ylabel('True Positive Rate or (Sensitivity)')
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+        plt.show()
+
+    return auc_score, precision, recall, specificity, npv, accuracy, f1
